003cb3220c31d2a7e6b8464304bba94744201e1a
SamIvanov7 <sam1206morfey@gmail.com>
2024-05-08 10:25:15 +0200

    update

--- js/image_zoom.js
+++ js/image_zoom.js
@@ -1,18 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     const images = document.querySelectorAll('.work-entry img');
-    const lightbox = document.getElementById('lightbox');
-    const lightboxImage = lightbox.querySelector('img');
+    const lightbox = document.createElement('div');
+    lightbox.className = 'lightbox';
+    document.body.appendChild(lightbox);
+
+    const imgTag = document.createElement('img');
+    lightbox.appendChild(imgTag);
 
     images.forEach(image => {
-        image.addEventListener('click', function () {
-            lightboxImage.src = this.src; // Set the src for the lightbox image
-            lightbox.style.display = 'flex'; // Show the lightbox
+        image.addEventListener('click', () => {
+            lightbox.style.display = 'flex';
+            imgTag.src = image.src;
         });
     });
 
-    // Click anywhere on the lightbox to close it
-    lightbox.addEventListener('click', function () {
-        lightbox.style.display = 'none'; // Hide the lightbox
-        lightboxImage.src = ''; // Optional: Clear the image source
+    lightbox.addEventListener('click', e => {
+        if (e.target !== imgTag) {
+            lightbox.style.display = 'none';
+        }
     });
 });
